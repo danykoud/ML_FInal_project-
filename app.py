3afcb68ac725207d204cd6f028d60ece8a4ecdf5
dany koudede <koudededany@icloud.com>
2022-07-12 12:01:31 -0500

    app improvement

--- app.py
+++ app.py
@@ -1,65 +1,53 @@
-# https://youtu.be/pI0wQbJwIIs
-"""
-
-Werkzeug provides a bunch of utilities for developing WSGI-compliant applications. 
-These utilities do things like parsing headers, sending and receiving cookies, 
-providing access to form data, generating redirects, generating error pages when 
-there's an exception, even providing an interactive debugger that runs in the browser. 
-Flask then builds upon this foundation to provide a complete web framework.
-"""
-
-from flask import Flask, render_template, request, redirect, flash
-from werkzeug.utils import secure_filename
-from main import getPrediction
 import os
+from flask import Flask, request, render_template
+import numpy as np
+import keras
+import tensorflow as tf
+from keras.models import load_model
 
-#Save images to the 'static' folder as Flask serves images from this directory
-UPLOAD_FOLDER = 'static/images/'
 
-#Create an app object using the Flask class. 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
-#Add reference fingerprint. 
-#Cookies travel with a signature that they claim to be legit. 
-#Legitimacy here means that the signature was issued by the owner of the cookie.
-#Others cannot change this cookie as it needs the secret key. 
-#It's used as the key to encrypt the session - which can be stored in a cookie.
-#Cookies should be encrypted if they contain potentially sensitive information.
-app.secret_key = "secret key"
+classes = ['Eczema', 'Vascular Tumors', 'Bullous Disease', 'Nail Fungus']
 
-#Define the upload folder to save images uploaded by the user. 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+new_model = load_model('Static/model/skin_train.h5')
 
-#Define the route to be home. 
-#The decorator below links the relative route of the URL to the function it is decorating.
-#Here, index function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
-@app.route('/')
+def predict(path):
+    new_model.summary()
+    test_image = keras.utils.load_img(path,target_size=(224,224))
+    test_image = tf.keras.utils.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis =0)
+    result = new_model.predict(test_image)
+    return result[0]
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-#Add Post method to the decorator to allow for form submission. 
-@app.route('/', methods=['POST'])
-def submit_file():
+@app.route("/submit", methods=["GET", "POST"])
+def upload():
+
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)  #Use this werkzeug method to secure filename. 
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            #getPrediction(filename)
-            label = getPrediction(filename)
-            flash(label)
-            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            flash(full_filename)
-            return redirect('/')
+	
+        img = request.files["my_image"]
+        if img.filename=="":
+            message='No file selected for uploading'
+            return render_template("index.html", message= message)
+
+        if img:
+            img_path = "static/" + img.filename	
+            img.save(img_path)
+    
+            pred= predict(img_path)
+
+            for i in range(3):
+                if pred[i] == 1.:
+                    break
+                prediction = classes[i]
+        
+
+            return render_template("index.html",img_path= img_path,  text=prediction)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug= True)
+  
